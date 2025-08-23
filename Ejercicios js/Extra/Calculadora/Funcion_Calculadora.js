24d24fb51e75ef11cb2c3e5e378bc50d10f20b7b
Leo <aguirreleo087@gmail.com>
2025-08-23 14:29:52 -0600

    Operaciones basicas con Numeros grandes

--- Ejercicios js/Extra/Calculadora/Funcion_Calculadora.js
+++ Ejercicios js/Extra/Calculadora/Funcion_Calculadora.js
@@ -14,38 +14,63 @@ function addValue(value){
     }else{
         realValue += '' + value;
     }
-
-    if (activeOperation == false){
+    showValues(realValue, value);
+    /*if (activeOperation == false){
         values[0] = parseInt(realValue);
         display.textContent = realValue;
     }else{
         values.push(parseInt(realValue))
         display.textContent += ' ' + realValue;
+    }*/
+}
+
+function valueZero(){
+    if (display.textContent == '0'){
+        realValue = 0;
     }
-    
+}
+
+function pushValue(pushingValue){
+    if (activeOperation == false){
+        values[0] = parseInt(pushingValue);
+    }else{
+        values.push(parseInt(pushingValue))
+    }
+    //showValues(pushingValue);
+}
+
+function showValues(fullValue, value){
+    if (activeOperation == false){
+        display.textContent = fullValue;
+    }else{
+        display.textContent += value;
+    }
 }
 
 function addOperation(operator){
-    display.textContent += ' ' + operator;
+    valueZero();
+    pushValue(realValue);
+    display.textContent += ' ' + operator + ' ';
     activeOperation = true;
     operation = operator;
     realValue = null;
 }
 
 function doOperation(){
-    if (operation != null){
+    pushValue(realValue);
+    if (values.length != 0 && operation != null){
         switch(operation){
             case '+':
-                result = values[0] + values[1]
+                result = values[0] + values[1];
                 break;
             case '-':
-                result = values[0] - values[1]
+                result = values[0] - values[1];
                 break;
             case 'x':
-                result = values[0] * values[1]
+                result = values[0] * values[1];
                 break;
             case '/':
-                result = values[0] / values[1]
+                result = values[0] / values[1];
                 break;
         }
     }else{
@@ -57,4 +82,13 @@ function doOperation(){
     realValue = values[0];
     activeOperation = false;
     operation = null;
+}
+
+function resetValue(){
+    values.length = 0;
+    realValue = null;
+    activeOperation = false;
+    operation = null;
+
+    display.textContent = 0;
 }
