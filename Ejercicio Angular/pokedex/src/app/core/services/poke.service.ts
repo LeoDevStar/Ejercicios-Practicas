9b31817d68bfa61528bb40ea9a98af767d530370
LeoDevStar <aguirreleo087@gmail.com>
2025-09-13 13:19:04 -0600

    Pokemon API URL

--- Ejercicio Angular/pokedex/src/app/core/services/poke.service.ts
+++ Ejercicio Angular/pokedex/src/app/core/services/poke.service.ts
@@ -1,8 +1,13 @@
+import { httpResource } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeService {
-  
+  readonly #pokeUrl = 'https://pokeapi.co/api/v2';
+
+  getPokeList() {
+    return httpResource(() => `${this.#pokeUrl}/pokemon?limit=151`);
+  }
 }
