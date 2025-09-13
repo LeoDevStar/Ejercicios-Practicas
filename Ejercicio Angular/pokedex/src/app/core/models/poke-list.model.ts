c8b27af81cb6f029433d84473511926f741213a6
LeoDevStar <aguirreleo087@gmail.com>
2025-09-13 13:29:31 -0600

    PokeResult Model Setup

--- Ejercicio Angular/pokedex/src/app/core/models/poke-list.model.ts
+++ Ejercicio Angular/pokedex/src/app/core/models/poke-list.model.ts
@@ -1,6 +1,8 @@
+import { PokeResult } from "./poke-result.model";
+
 export interface PokeList{
     count: number;
     next: string;
     previous: string;
-    results: unknown[];
+    results: PokeResult[];
 }
