0a2a21b731312f683972c1324143f2c0626de0e8
LeoDevStar <aguirreleo087@gmail.com>
2025-09-18 01:17:38 -0600

    PokeService getPokemon Function Setup and Pokemon Model Setup

--- Ejercicio Angular/pokedex/src/app/core/services/poke.service.ts
+++ Ejercicio Angular/pokedex/src/app/core/services/poke.service.ts
@@ -1,5 +1,5 @@
 import { httpResource, HttpResourceRef } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, Signal } from '@angular/core';
 import { PokeList } from '../models/poke-list.model';
 
 @Injectable({
@@ -10,5 +10,9 @@ export class PokeService {
 
   getPokeList(): HttpResourceRef<PokeList | undefined> {
     return httpResource<PokeList>(() => `${this.#pokeUrl}/pokemon?limit=151`);
+  }
+
+  getPokemon(name: Signal<string>){
+    return httpResource(() => `${this.#pokeUrl}/pokemon/${name()}`);
   }
 }
