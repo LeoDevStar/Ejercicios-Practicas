b695e42e62d9b7d89edcf80a963bd0e5d7db9725
LeoDevStar <aguirreleo087@gmail.com>
2025-09-18 00:55:39 -0600

    Poke Card RouterLink to Details

--- Ejercicio Angular/pokedex/src/app/list/card/card.ts
+++ Ejercicio Angular/pokedex/src/app/list/card/card.ts
@@ -2,10 +2,11 @@ import { Component, input } from '@angular/core';
 import { PokeResult } from '../../core/models/poke-result.model';
 import { NgOptimizedImage, TitleCasePipe } from '@angular/common';
 import { PokeImgPipe } from './poke-img-pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card',
-  imports: [TitleCasePipe, PokeImgPipe, NgOptimizedImage],
+  imports: [TitleCasePipe, PokeImgPipe, NgOptimizedImage, RouterLink],
   templateUrl: './card.html',
   styleUrl: './card.scss'
 })
