03afcbb305b1be1a46dc57282402c8de414db3be
LeoDevStar <aguirreleo087@gmail.com>
2025-09-18 00:17:28 -0600

    Card TitleCasePipe

--- Ejercicio Angular/pokedex/src/app/list/card/card.ts
+++ Ejercicio Angular/pokedex/src/app/list/card/card.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { PokeResult } from '../../core/models/poke-result.model';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [TitleCasePipe],
   templateUrl: './card.html',
   styleUrl: './card.scss'
 })
