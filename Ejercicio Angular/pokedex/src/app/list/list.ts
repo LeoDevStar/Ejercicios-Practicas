51ae121950d45791e1ae7f735e2bdccc6bd9c897
LeoDevStar <aguirreleo087@gmail.com>
2025-09-18 10:18:45 -0600

    Search Bar HTML and Integration to List

--- Ejercicio Angular/pokedex/src/app/list/list.ts
+++ Ejercicio Angular/pokedex/src/app/list/list.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { PokeService } from '../core/services/poke.service';
 import { Card } from './card/card';
+import { SearchBar } from '../search-bar/search-bar';
 
 @Component({
   selector: 'app-list',
-  imports: [Card],
+  imports: [Card, SearchBar],
   templateUrl: './list.html',
   styleUrl: './list.scss'
 })
