b9d325e9cf56c8ee8b54268989ba8c29118ebc01
LeoDevStar <aguirreleo087@gmail.com>
2025-09-20 14:28:57 -0600

    Code Refactor, Parenthesis Buttons and doOperation Function

--- Ejercicios js/Extra/Calculadora/Funcion_Calculadora.js
+++ Ejercicios js/Extra/Calculadora/Funcion_Calculadora.js
@@ -1,157 +1,207 @@
+
+
+class Operacion{
+    constructor(){
+        this.numeros = [];
+        this.operador = "";
+        this.prioridad = 0;
+    }
+
+    agregarNumero(numero){
+        this.numeros.push(numero);
+    }
+
+    agregarOperador(operador){
+        this.operador = operador;
+    }
+
+    definirPrioridad(){
+        if(this.operador != ""){
+            switch (this.operador) {
+                case "+":
+                    this.prioridad = 0;
+                    break;
+                case "-":
+                    this.prioridad = 0;
+                    break;
+                case 'x':
+                    this.prioridad = 1;
+                    break;
+                case '/':
+                    this.prioridad = 1;
+                    break;
+                default:
+                    break;
+            }
+        }else{
+            return "No hay operador definido";
+        }
+        
+    }
+
+    resultado(){
+        if(this.numeros.length == 2){
+            if (this.operador != ""){
+                switch (this.operador) {
+                    case "+":
+                        return this.numeros[0] + this.numeros[1];
+                        break;
+                    case "-":
+                        return this.numeros[0] - this.numeros[1];
+                        break;
+                    case "x":
+                        return this.numeros[0] * this.numeros[1];
+                        break;
+                    case "/":
+                        return this.numeros[0] / this.numeros[1];
+                        break;
+                    default:
+                        break;
+                }
+            }else{
+                return "no hay operador definido"
+            }
+        }else{
+            return "no hay suficientes numeros para realizar la operacion";
+        }
+    }
+
+
+}
+
 var display = document.getElementById("texto");
-
-let operationData = [];
-let realNumber = '';
-var loadValues = [];
-
-var parenthesisAdded = false;
-var doingSubOperation = false;
-var subOperation = [];
-var subArray;
-
-let actualArray = [];
-var operationArray = [];
-var operatorsArray = [];
-let resultsArray = [];
+var firstNumber = true;
+var doingOperation = false;
+var stringNumber = "";
+var integerNumber;
+var operationData = [];
+var subOperationData = [];
 
 
+var resultado = 0;
 
-var preOperationResult;
-var operationResult = 0;
-let doingOperation = false;
-
-let activeOperator;
-let subOperator;
 
 
 function addNumber(number){
-    
-    if(realNumber == ''){
-        realNumber = number;
+
+    if (firstNumber){
+        stringNumber = number;
+        firstNumber = false;
     }else{
-        realNumber += number;
+        stringNumber += number;
     }
     
-    if (doingOperation){
+    if(!doingOperation){
+        display.textContent = stringNumber;
+    }else{
         display.textContent += number;
-    }else{
-        display.textContent = realNumber;
     }
     
+    
+}
+
+function addNumberToOperationData(numberToAdd){
+    integerNumber = parseInt(numberToAdd);
+    operationData.push(integerNumber);
+    firstNumber = true;
 }
 
 function addOperator(operator){
-    
-    if(operator == "()"){
-        if (!parenthesisAdded){
-            parenthesisAdded = true;
-            operator = '(';
-        }else{
-            parenthesisAdded = false;
-            operator = ')';
-        }
-    }
+    if(stringNumber != ""){
+        addNumberToOperationData(stringNumber);
 
-    if(realNumber != ''){
-        console.log("Pushing real number: " + realNumber);
-        operationData.push(parseInt(realNumber));
-        
-        realNumber = '';
-    }
+        operationData.push(operator);
 
-    operationData.push(operator);
-    display.textContent += ' ' + operator + ' ';
-    doingOperation = true;
+        display.textContent += " " + operator + " ";
+        doingOperation = true;
+        stringNumber = "";
+    }
 }
 
+function addParenthesis(value){
+    if(stringNumber != ""){
+        addNumberToOperationData(stringNumber);
 
-function newOperation(){
-    if(realNumber != ''){
-        operationData.push(parseInt(realNumber));
+        doingOperation = true;
+        stringNumber = "";
     }
+
+    if(value == 'p'){
+        value = "(";
+    }else if (value == '/p'){
+        value = ")";
+    }
+
+    operationData.push(value);
+
+    display.textContent += " " + value + " ";
+
     
-    if (operationData.length >= 2){    
+}
+
+function solveOperation(operationArray){
+    let i = 0;
+    var operacion;
+    let prioridad = 1;
+
+    let subOperationData;
+    let subOperationResult;
+
+
+    if (operationArray.findIndex(element => element == "(") != -1){
+        subOperationData = operationArray.slice(
+            operationArray.findIndex(element => element == "(")+1,
+            operationArray.findLastIndex(element => element == ")")
+        );
+        subOperationResult = solveOperation(subOperationData);
+        operationArray.splice(operationArray.findIndex(element => element == "("), operationArray.findLastIndex(element => element == ")"), subOperationResult);
+    }
         
-        operationData.forEach(value =>{
+    do{
+        if(!(operationArray[i] >= 0)){
 
-            if (value>=0 && actualArray.length < 2){                
-                actualArray.push(value);
-            }else{
-                if(value == '('){
-                    
-                    operationArray.push(actualArray);
-                    
-                    actualArray = new Array();
+            if(operationArray[i] != "(" && operationArray[i] != ")"){
+                operacion = new Operacion();
+                operacion.agregarNumero(operationArray[i-1]);
+                operacion.agregarOperador(operationArray[i]);
+                operacion.definirPrioridad();
+                operacion.agregarNumero(operationArray[i+1]);
+                console.log("operacion: " + operacion.numeros[0] + operacion.operador + operacion.numeros[1] + " prioridad: " + operacion.prioridad);
+            }
+            
+            
+            
 
-                }else if (value == ')'){
-                    console.log("SubOperation False");
-                    
-                    operationArray[0] = [...operationArray[0], ...actualArray];
-                    
-                    actualArray = operationArray.at(-1);
-                    
-                    operatorsArray.pop();
-
-                    operationArray.pop();
-                    
-                }else{
-                    operatorsArray.push(value);
-                }
+            if(operacion.prioridad == prioridad){   
+                operationArray.splice(i-1,3,operacion.resultado());  
+                i = 0;
             }
 
-            doOperation(actualArray, operatorsArray.at(-1));
-            
-        })
-        
-        operationResult = actualArray[0];
-        actualArray.length = 0;
-        operationArray.length = 0;
-        operatorsArray.length = 0;
-    }else{
-        operationResult = operationData[0];
-        
-    }
-    
-    display.textContent = operationResult.toString();
-    
-    
-    operationData.length = 0;
-    realNumber = operationResult.toString();
 
-    doingOperation = false;
-    activeOperator = null;
-    subOperator = null;
-}
+        }
+        
+        i += 1;
 
-function doOperation(valueArray, operator){
-    
-    if(valueArray.length == 2){
-        console.log("Doing calc");
-        console.log(valueArray[0]);
-        console.log(operator);
-        console.log(valueArray[1]);
-        switch (operator) {
-            case '+':
-                preOperationResult = valueArray[0] + valueArray[1];
-                break;
-            case '-':
-                preOperationResult = valueArray[0] - valueArray[1];
-                break;
-            case 'x':
-                preOperationResult = valueArray[0] * valueArray[1];
-                break;
-            case '/':
-                preOperationResult = valueArray[0] / valueArray[1];
-                break;
-            default:
-                break;
+        if(i > operationArray.length){
+            prioridad -= 1;
+            i = 0;
         }
 
-        console.log("preOperationResult: " + preOperationResult);
-                
-        valueArray.length = 0;          
-        valueArray[0] = preOperationResult;    
-        
-    }
-    
+    }while(operationArray.length > 1);
+
+    return operationArray[0];
 }
+
+function doOperation(){
+    if(stringNumber != ""){
+        addNumberToOperationData(stringNumber);
+    }
+
+    resultado = solveOperation(operationData);
+    display.textContent = resultado.toString();
+    doingOperation = false;
+}
+
+
+
+
