43e770239d98812e1ecc4c116cb1badd1697330f
LeoDevStar <aguirreleo087@gmail.com>
2025-09-23 10:39:02 -0600

    Pokemon Card Details

--- Ejercicio Angular/pokedex/src/app/list/card/card.ts
+++ Ejercicio Angular/pokedex/src/app/list/card/card.ts
@@ -1,4 +1,5 @@
-import { Component, input } from '@angular/core';
+import { Component, input, inject } from '@angular/core';
+import { PokeService } from '../../core/services/poke.service';
 import { PokeResult } from '../../core/models/poke-result.model';
 import { NgOptimizedImage, TitleCasePipe } from '@angular/common';
 import { PokeImgPipe } from './poke-img-pipe';
@@ -11,5 +12,10 @@ import { RouterLink } from '@angular/router';
   styleUrl: './card.scss'
 })
 export class Card {
-readonly pokeResult = input.required<PokeResult>();
+
+  readonly #pokeService = inject(PokeService);
+  readonly pokeResult = input.required<PokeResult>();
+  readonly name = input.required<string>();
+
+  protected readonly pokeResource = this.#pokeService.getPokemon(this.name);
 }
