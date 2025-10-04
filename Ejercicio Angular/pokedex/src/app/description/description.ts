915f910f5ac633c47133df4af0c612cdd351bd68
LeoDevStar <aguirreleo087@gmail.com>
2025-10-04 12:13:20 -0600

    Info Types Change Description

--- Ejercicio Angular/pokedex/src/app/description/description.ts
+++ Ejercicio Angular/pokedex/src/app/description/description.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { PokeService } from '../core/services/poke.service';
 
 @Component({
   selector: 'app-description',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './description.scss'
 })
 export class Description {
-
+  readonly name = input<string>('');
+  readonly #pokeService = inject(PokeService);
+  protected readonly pokeSpeciesResource = this.#pokeService.getPokemonSpecies(this.name);
 }
--- Ejercicio Angular/pokedex/src/app/info/types/types.ts
+++ Ejercicio Angular/pokedex/src/app/info/types/types.ts
@@ -1,25 +1,23 @@
 import { Component, inject, input } from '@angular/core';
 import { PokeService } from '../../core/services/poke.service';
 import { Image } from '../../image/image';
+import { Description } from '../../description/description';
 
 @Component({
   selector: 'app-types',
-  imports: [Image],
+  imports: [Image, Description],
   templateUrl: './types.html',
   styleUrl: './types.scss'
 })
 export class Types {
   readonly evo_url = input<string>('');
-  readonly name = input<string>('');
   readonly #pokeService = inject(PokeService);
-
   protected readonly pokeEvolution_Chain = this.#pokeService.getPokemonEvolutionChain(this.evo_url);
 
-  pokemonPointer = this.pokeEvolution_Chain.value()?.chain;
-  
-  getPokemonEvolution(){
-    while(this.pokemonPointer?.evolves_to.at(0) != undefined){
-      this.pokemonPointer = this.pokemonPointer?.evolves_to.at(0);
-    }
+  description = 0;
+
+  switchDescription(desc: number){
+    this.description = desc;
   }
+  
 }
