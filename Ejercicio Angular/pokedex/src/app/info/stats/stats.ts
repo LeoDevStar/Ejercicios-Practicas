bc7ccc6599b57a4b581cc1d67fc77603d07c2620
LeoDevStar <aguirreleo087@gmail.com>
2025-10-07 10:59:49 -0600

    Info Stats Dynamic Styling

--- Ejercicio Angular/pokedex/src/app/info/stats/stats.ts
+++ Ejercicio Angular/pokedex/src/app/info/stats/stats.ts
@@ -1,10 +1,10 @@
 import { Component, inject, input } from '@angular/core';
 import { PokeService } from '../../core/services/poke.service';
-import { TitleCasePipe } from '@angular/common';
+import { NgClass, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-stats',
-  imports: [TitleCasePipe],
+  imports: [TitleCasePipe, NgClass],
   templateUrl: './stats.html',
   styleUrl: './stats.scss'
 })
@@ -13,4 +13,32 @@ export class Stats {
   readonly name = input<string>('');
 
   protected readonly pokeResource = this.#pokeService.getPokemon(this.name);
+
+  applyStyle(stat_name: string){
+    let class_name = '';
+    switch (stat_name) {
+      case "Hp":
+        class_name = 'hp-stat';
+        break;
+      case "Attack":
+        class_name = 'attack-stat';
+        break;
+      case "Defense":
+        class_name = 'defense-stat';
+        break;
+      case "Special-attack":
+        class_name = 'special-attack-stat';
+        break;
+      case "Special-defense":
+        class_name = 'special-defense-stat';
+        break;
+      case "Speed":
+        class_name = 'speed-stat';
+        break;
+      default:
+        class_name = 'default-stat';
+        break;
+    }
+    return class_name;
+  }
 }
