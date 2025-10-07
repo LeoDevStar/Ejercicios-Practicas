5a35128d1a954f883b4458d0fb208cf379621de0
LeoDevStar <aguirreleo087@gmail.com>
2025-10-07 11:20:58 -0600

    Poke Card Details Order and Forms and Types Switch Content

--- Ejercicio Angular/pokedex/src/app/info/forms/forms.ts
+++ Ejercicio Angular/pokedex/src/app/info/forms/forms.ts
@@ -1,17 +1,28 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { PokeService } from '../../core/services/poke.service';
 import { TitleCasePipe } from '@angular/common';
 import { Image } from '../../image/image';
+import { Description } from '../../description/description';
 
 @Component({
   selector: 'app-forms',
-  imports: [TitleCasePipe, Image],
+  imports: [Description, Image],
   templateUrl: './forms.html',
   styleUrl: './forms.scss'
 })
 export class Forms {
-  readonly name = input<string>('');
+  readonly evo_url = input<string>('');
   readonly #pokeService = inject(PokeService);
+  protected readonly pokeEvolution_Chain = this.#pokeService.getPokemonEvolutionChain(this.evo_url);
 
-  protected readonly pokeResource = this.#pokeService.getPokemon(this.name);
+  description = 0;
+  poke_name = output<string>();
+
+  switchDescription(desc: number){
+    this.description = desc;
+  }
+
+  switchPokeName(name: string){
+    this.poke_name.emit(name);
+  }
 }
--- Ejercicio Angular/pokedex/src/app/info/types/types.ts
+++ Ejercicio Angular/pokedex/src/app/info/types/types.ts
@@ -2,27 +2,18 @@ import { Component, inject, input, output } from '@angular/core';
 import { PokeService } from '../../core/services/poke.service';
 import { Image } from '../../image/image';
 import { Description } from '../../description/description';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-types',
-  imports: [Image, Description],
+  imports: [Image, TitleCasePipe],
   templateUrl: './types.html',
   styleUrl: './types.scss'
 })
 export class Types {
-  readonly evo_url = input<string>('');
+  readonly name = input<string>('');
   readonly #pokeService = inject(PokeService);
-  protected readonly pokeEvolution_Chain = this.#pokeService.getPokemonEvolutionChain(this.evo_url);
 
-  description = 0;
-  poke_name = output<string>();
-
-  switchDescription(desc: number){
-    this.description = desc;
-  }
-
-  switchPokeName(name: string){
-    this.poke_name.emit(name);
-  }
+  protected readonly pokeResource = this.#pokeService.getPokemon(this.name);
   
 }
